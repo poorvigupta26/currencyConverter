193f50cb1ed0a729743415d1cc55ec36e4319960
Poorvi <151968078+poorvigupta26@users.noreply.github.com>
2025-04-12 01:27:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+ for(let select of dropdowns){
+    for( currCode in countryList){
+       let newOption = document.createElement("option");
+       newOption.innerText = currCode;
+       newOption.value = currCode;
+       if(select.name == "from" && currCode=="USD"){
+        newOption.selected = "selected";
+       } else if(select.name == "to" && currCode=="INR"){
+            newOption.selected = "selected";
+       }
+       select.append(newOption);
+    }
+    select.addEventListener("change", (evt) =>{
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag= (element)=>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newURL = `https://flagsapi.com/I${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newURL;
+};
+
+btn.addEventListener("click", async (evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector("form input");
+    let amountValue = amount.value;
+    if(amountValue==="" || amountValue<1){
+        amountValue = 1;
+        amount.value = "1";
+    }
+    let fromCurrLower = fromCurr.value.toLowerCase();
+    let toCurrLower = toCurr.value.toLowerCase()
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`
+    let response = await fetch(URL);
+    let data = await  response.json();
+    let rate = data[fromCurrLower][toCurrLower];
+    let finalAmount = amountValue * rate;
+    msg.innerText = `${amountValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+});
